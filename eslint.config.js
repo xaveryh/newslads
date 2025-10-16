374cbe55f0061d219dd5f372c0002e65938de5f3
harry-swi <chorn.chhunheang123@gmail.com>
2025-10-16 17:55:32 +1100

    Editing the eslint config and start working on rule.

--- eslint.config.js
+++ eslint.config.js
@@ -2,28 +2,42 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import babelParser from '@babel/eslint-parser'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'build', 'coverage', 'node_modules','index.html']),
   {
     files: ['**/*.{js,jsx}'],
+    ignores: [
+      'eslint.config.js',
+      'vite.config.*',
+      'tailwind.config.{js,cjs,ts}',
+      'postcss.config.{js,cjs,ts}',
+      'scripts/**'
+    ],
     extends: [
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: { ...globals.browser },
+      parser: babelParser,
+      parserOptions: { 
+        requireConfigFile: false,
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+        babelOptions: {
+          presets: [['@babel/preset-react', {runtime: 'automatic'}]]
+        }
+      }
     },
+
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': ['error', { varsIgnorePattern: '[A-Z]', argsIgnorePattern: '^_' }],
+      'no-undef': 'error'
     },
   },
 ])
